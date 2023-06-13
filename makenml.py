bd2b7d0038491699597d907c1589635c17a8b179
danttt <kaseijin@posteo.nz>
2023-06-13 21:24:39 +0200

    Bugfixing house type town hall

--- makenml.py
+++ makenml.py
@@ -65,6 +65,13 @@ sections.append(church.read())
 church.close()
 
 
+# Open the file containing the nml-code for this house type and append it to the "sections" list
+print ("Appending house type 'town_hall'...")
+town_hall = codecs.open("pnml/town_hall.pnml", 'r', 'utf8')
+sections.append(town_hall.read())
+town_hall.close()
+
+
 # Create the final .nml-file and make it writable
 # Then write the "sections" list into 'cet.nml' using new lines for each part
 print ("Creating NML file...")
