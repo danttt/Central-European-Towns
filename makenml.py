1dbc6bba254ce330e4ff837cab6da54b04a0869f
danttt <kaseijin@posteo.nz>
2023-06-18 17:52:17 +0200

    Added house type alley houses for towns

--- makenml.py
+++ makenml.py
@@ -135,6 +135,13 @@ sections.append(farm.read())
 farm.close()
 
 
+# Open the file containing the nml-code for this house type and append it to the "sections" list
+print ("Appending house type 'alley houses'...")
+alley_houses = codecs.open("pnml/alley_houses.pnml", 'r', 'utf8')
+sections.append(alley_houses.read())
+alley_houses.close()
+
+
 # Create the final .nml-file and make it writable
 # Then write the "sections" list into 'cet.nml' using new lines for each part
 print ("Creating NML file...")
