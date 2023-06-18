20616fdd5b8e439797821c54ac7e7d1935baa6d6
danttt <kaseijin@posteo.nz>
2023-06-18 22:40:47 +0200

    Added house type trading house

--- makenml.py
+++ makenml.py
@@ -156,6 +156,13 @@ sections.append(old_town_house.read())
 old_town_house.close()
 
 
+# Open the file containing the nml-code for this house type and append it to the "sections" list
+print ("Appending house type 'trading house'...")
+trading_house = codecs.open("pnml/trading_house.pnml", 'r', 'utf8')
+sections.append(trading_house.read())
+trading_house.close()
+
+
 # Create the final .nml-file and make it writable
 # Then write the "sections" list into 'cet.nml' using new lines for each part
 print ("Creating NML file...")
