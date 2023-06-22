9b461acb6c59ac46ca756bd40851c9378ee09888
danttt <kaseijin@posteo.nz>
2023-06-22 19:27:48 +0200

    Added house type hospital

--- makenml.py
+++ makenml.py
@@ -233,6 +233,13 @@ sections.append(stadium.read())
 stadium.close()
 
 
+# Open the file containing the nml-code for this house type and append it to the "sections" list
+print ("Appending house type 'hospital'...")
+hospital = codecs.open("pnml/hospital.pnml", 'r', 'utf8')
+sections.append(hospital.read())
+hospital.close()
+
+
 # Create the final .nml-file and make it writable
 # Then write the "sections" list into 'cet.nml' using new lines for each part
 print ("Creating NML file...")
