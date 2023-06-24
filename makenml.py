dd8d2ef105c3a903c8f8fd50e36c66f17d4124ff
danttt <kaseijin@posteo.nz>
2023-06-24 23:42:24 +0200

    Added road aware house type shops offices 3

--- makenml.py
+++ makenml.py
@@ -240,6 +240,13 @@ sections.append(hospital.read())
 hospital.close()
 
 
+# Open the file containing the nml-code for this house type and append it to the "sections" list
+print ("Appending house type 'shops and offices 3'...")
+shops_offices_3 = codecs.open("pnml/shops_offices_3.pnml", 'r', 'utf8')
+sections.append(shops_offices_3.read())
+shops_offices_3.close()
+
+
 # Create the final .nml-file and make it writable
 # Then write the "sections" list into 'cet.nml' using new lines for each part
 print ("Creating NML file...")
